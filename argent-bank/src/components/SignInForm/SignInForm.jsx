75a6832721ed03d679f28c0deedcb737e5a208ef
emnificent <github.com/emnificent>
2023-02-10 16:36:20 +0100

    fix: remove confusion for react

--- argent-bank/src/components/SignInForm/SignInForm.jsx
+++ argent-bank/src/components/SignInForm/SignInForm.jsx
@@ -4,15 +4,15 @@ function SignInForm() {
       <h1 className='sign-in-form__title'>Sign In</h1>
       <form>
         <div className='sign-in-form__text-input'>
-          <label for='username'>Username</label>
+          <label htmlFor='username'>Username</label>
           <input type='text' id='username' />
         </div>
         <div className='sign-in-form__text-input'>
-          <label for='password'>Password</label>
+          <label htmlFor='password'>Password</label>
           <input type='password' id='password' />
         </div>
         <div className='sign-in-form__checkbox'>
-          <label for='remember'>Remember me</label>
+          <label htmlFor='remember'>Remember me</label>
           <input type='checkbox' id='remember' />
         </div>
         <button className='sign-in-form__button primary-button'>Sign In</button>
