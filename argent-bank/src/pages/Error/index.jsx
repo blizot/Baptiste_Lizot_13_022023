c27a7dc8c9a967ab89cf208fe7364e2a6ee1250f
emnificent <github.com/emnificent>
2023-02-10 17:52:27 +0100

    feat: improve error page

--- argent-bank/src/pages/Error/index.jsx
+++ argent-bank/src/pages/Error/index.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from 'react-router-dom'
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom'
 
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
@@ -7,18 +7,22 @@ function Error() {
   const error = useRouteError()
   console.error(error)
 
-  return (
-    <>
-      <Header />
-      <main className='error'>
-        <h1 className='error__title'>{error.status === 404 ? error.status : 'Oops!'}</h1>
-        <p>{error.status === 404 ? 'This page doesn\'t exist' : 'Sorry, an unexpected error has occured:'}</p>
-        {error.status !== 404 && <p className='error__message'>{error.statusText || error.message}</p>}
-        <a href='./' className='error__link primary-button'>Go back to home page</a>
-      </main>
-      <Footer />
-    </>
-  )
+  if (isRouteErrorResponse(error)) {
+    return (
+      <>
+        <Header />
+        <main className='error'>
+          <h1 className='error__title'>{error.status === 404 ? error.status : 'Oops!'}</h1>
+          <p>{error.status === 404 ? 'This page doesn\'t exist' : 'Sorry, an unexpected error has occured:'}</p>
+          {error.status !== 404 && <p className='error__message'>{error.statusText || error.message}</p>}
+          <a href='./' className='error__link primary-button'>Go back to home page</a>
+        </main>
+        <Footer />
+      </>
+    )
+  } else {
+    return <p className='error__message--colored'>Oops!</p>
+  }
 }
 
 export default Error
