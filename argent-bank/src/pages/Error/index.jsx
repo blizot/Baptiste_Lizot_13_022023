0a8ba66e5647235f19d12b53a12c903ab3e6a8f2
emnificent <github.com/emnificent>
2023-02-19 14:24:44 +0100

    refactor: clean code of Error page/component

--- argent-bank/src/pages/Error/index.jsx
+++ argent-bank/src/pages/Error/index.jsx
@@ -7,24 +7,24 @@ function Error() {
   const error = useRouteError()
   console.error(error)
 
-  if (isRouteErrorResponse(error)) {
-    return (
-      <>
-        <Header />
-        <main className='error'>
-          <h1 className='error__title'>{error.status === 404 ? error.status : 'Oops!'}</h1>
-          <p>{error.status === 404 ? 'This page doesn\'t exist' : 'Sorry, an unexpected error has occured:'}</p>
-          {error.status !== 404 && 
-            <p className='error__message'>{error.statusText || error.message}</p>
-          }
-          <Link to='/' className='error__link primary-button'>Go back to home page</Link>
-        </main>
-        <Footer />
-      </>
-    )
-  } else {
+  if (!isRouteErrorResponse(error)) { 
     return <p className='error__message error__message--colored'>Oops!</p>
   }
+
+  return (
+    <>
+      <Header />
+      <main className='error'>
+        <h1 className='error__title'>{error.status === 404 ? error.status : 'Oops!'}</h1>
+        <p>{error.status === 404 ? 'This page doesn\'t exist' : 'Sorry, an unexpected error has occured:'}</p>
+        {error.status !== 404 && 
+          <p className='error__message'>{error.statusText || error.message}</p>
+        }
+        <Link to='/' className='error__link primary-button'>Go back to home page</Link>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default Error
